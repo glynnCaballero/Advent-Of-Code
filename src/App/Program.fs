faac1426c78568ce2d1bab502fcc5ca030b3b675
glynnCaballero <caballeroglynn@gmail.com>
2024-11-10 15:55:47 +1000

    chorse: timer function updates

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,15 +1,12 @@
 ﻿open System
+open Utilities
 
 [<EntryPoint>]
 let main args =
     // printf "%d" args.Length
     let input = if args.Length = 1 then $".\\problems\\{args[0]}.txt"  else ".\\problems\\day5Demo.txt"
-
-    let stopWatch = Diagnostics.Stopwatch.StartNew()
     
-    Solutions.Day5.solve input
-
-    stopWatch.Stop();
-    printf "\n ------- Total Time ------- \n %d milliseconds" stopWatch.Elapsed.Milliseconds
+    RunWithTimer (fun _ -> Solutions.Day5.solve input) 
+     
 
     0 // return an integer exit code
---
+++ src/Solutions/Utilities.fs
@@ -0,0 +1,11 @@
+﻿module Utilities
+
+open System;
+
+let RunWithTimer a =
+    let stopWatch = Diagnostics.Stopwatch.StartNew()
+
+    let _ = a ()
+
+    stopWatch.Stop()
+    printf "\n ------- Total Time ------- \n %d milliseconds" stopWatch.Elapsed.Milliseconds
